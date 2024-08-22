12877611c944432db151144885e566abfbc88bc2
ynparmar07 <ymparmar7@gmail.com~>
2024-08-22 15:44:40 +0530

    first commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,14 @@
+setTimeout(function() {
+  console.log("I am living large and taking charge! \nI'm not concerned about my age;\nI don't feel a day over fabulous!\nQuit looking for a reason to dislike me;\nI'm just naturally likable.\nThankfully, my favorite things in life don't cost money.\nYou go do what you will do, and I'll go do what's best for me.\nI have to be successful because I have very expensive taste.\nI can count the number of girlfriend I've had on one hand (if I use a calculator) \nLove me, hate me - either way, you're spending your time thinking of me \n!Caution: You might get addicted to me.\nYou couldn't handle me, even if I came with instructions.\nIf you're cooler than me, does that make me hotter than you?\nIf time is money, I am running out of time.\nI'm half princess, half ninja; deal with it!\n!Warning: I have an attitude, and I know how to use it!\nI'm so hot, I make fire!\nI'm not ashamed to say I've never loved another person as much as I love myself ");
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hii, I am Y.M.PARMAR </span> As a passionate website developer with  years of experience, I specialize in creating dynamic, user-friendly websites that blend creativity with cutting-edge technology. My expertise spans front-end and back-end development, ensuring that every project I work on is not only visually appealing but also optimized for performance and scalability. Whether it's crafting responsive designs or developing custom functionalities, I thrive on turning ideas into engaging digital experiences. I'm committed to staying updated with the latest trends and best practices in web development, enabling me to deliver solutions that are both innovative and effective. Let's work together to bring your vision to life on the web. What truly sets me apart is my dedication to understanding my clients' unique needs and visions. I believe that a successful website begins with a deep dive into the customer's requirements, so I take the time to listen, ask the right questions, and truly understand what you're looking for. By aligning my technical skills with your business goals, I ensure that the final product not only meets but exceeds your expectations. Let's collaborate to create a web presence that perfectly represents your brand and drives results"
+        
+      ],
+      typeSpeed: 7,
+      contentType: 'html',
+      
+    });
+
+  }, 100);
+
